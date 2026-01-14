6b5095b3122841c64ddf086548404c89d99e59de
unknown <banhquanghuy11@gmail.com>
2026-01-14 16:50:52 +0700

    cai app

--- main/templates/main/sw.js
+++ main/templates/main/sw.js
@@ -1,7 +1,6 @@
 const CACHE_NAME = 'bqh-movie-v1';
 const urlsToCache = [
   '/',
-  '/static/css/style.css', // Nếu bạn có file css riêng
   '/static/images/icon-192.png',
   '/static/images/icon-512.png'
 ];
@@ -10,6 +9,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
+        // Sử dụng return để đảm bảo cache xong mới kết thúc install
         return cache.addAll(urlsToCache);
       })
   );
