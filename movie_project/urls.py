1824c58b7cc2bbbfc4b6a625625863c1d0dd3798
unknown <banhquanghuy11@gmail.com>
2026-01-14 18:47:11 +0700

    Fix TemplateView NameError and add webpush migrations

--- movie_project/urls.py
+++ movie_project/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView, TemplateView
+
 
 urlpatterns = [
     # Quản trị hệ thống
